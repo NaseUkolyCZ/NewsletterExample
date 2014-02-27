24e309953356bd2e277da19bfdd9c0607ad08599
davidpodhola <david.podhola@naseukoly.cz>
2014-02-27 22:51:41 +0100

    sql script to create the db table

---
+++ Create dbo.Recipients.sql
@@ -0,0 +1,19 @@
+﻿USE [Newsletter]
+GO
+
+/****** Object: Table [dbo].[Recipients] Script Date: 2/27/2014 10:50:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Recipients] (
+    [SmtpAddress]     NVARCHAR (MAX) NOT NULL,
+    [RecipientName]   NVARCHAR (MAX) NOT NULL,
+    [FirstProvidedBy] NVARCHAR (MAX) NOT NULL,
+    [EmailDomain]     NVARCHAR (MAX) NULL,
+    [Excluded]        BIT            NULL
+);
+
+

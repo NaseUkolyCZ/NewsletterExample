91c1213e179923c04ab9e742309d26e22c9afc66
davidpodhola <david.podhola@naseukoly.cz>
2014-03-04 06:47:07 +0100

    sp_InsertOrUpdate

--- Create dbo.Recipients.sql
+++ Create dbo.Recipients.sql
@@ -4,6 +4,9 @@ CREATE TABLE [dbo].[Recipients] (
     [SmtpAddress]     NVARCHAR (255) NOT NULL,
     [RecipientName]   NVARCHAR (1024) NOT NULL,
     [FirstProvidedBy] NVARCHAR (255) NOT NULL,
+	[SampleSubject1]     NVARCHAR (255) NULL,
+	[SampleSubject2]     NVARCHAR (255) NULL,
+	[SampleSubject3]     NVARCHAR (255) NULL,
     [EmailDomain]     NVARCHAR (255) NULL,
     [Excluded]        BIT            NULL, 
     CONSTRAINT [PK_Recipients] PRIMARY KEY ([SmtpAddress])
---
+++ Create dbo.sp_InsertOrUpdate.sql
@@ -0,0 +1,37 @@
+﻿USE [Newsletter]
+GO
+
+/****** Object: SqlProcedure [dbo].[sp_InsertOrUpdate] Script Date: 4. 3. 2014 6:44:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE sp_InsertOrUpdate
+	@smtpAddress nvarchar(max), 
+	@RecipientName nvarchar(max) 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	begin tran
+	   IF EXISTS (SELECT 1 FROM [dbo].[Recipients]  WHERE smtpAddress = @smtpAddress)
+	   begin
+		 update [dbo].[Recipients] with (serializable) set RecipientName = @RecipientName where smtpAddress = @smtpAddress
+		 and @RecipientName <> @smtpAddress
+	   end
+	   else
+	   begin
+		  insert [dbo].[Recipients] (RecipientName, smtpAddress, FirstProvidedBy ) values (@RecipientName,@smtpAddress, SYSTEM_USER)
+	   end
+	commit tran
+
+END
